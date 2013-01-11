49583af0af0d6a80061ebc712acdf00977af466e
Tbo <thibault@official.fm>
2013-01-11 14:54:11 +0100

    file encoding

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,4 @@
+# -*- encoding : utf-8 -*-
 # Be sure to restart your server when you modify this file.
 
 IphotographismeV2::Application.config.session_store :cookie_store, key: '_iphotographisme_v2_session'
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
+# -*- encoding : utf-8 -*-
 IphotographismeV2::Application.routes.draw do
   root to: 'pictures#index'
 end

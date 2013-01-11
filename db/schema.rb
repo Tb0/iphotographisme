fb420c826e1f5af585bc8b2447aab017c509c345
Tbo <thibault@official.fm>
2013-01-11 14:24:54 +0100

    Add picture migration,model,factory

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,24 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130111130216) do
+ActiveRecord::Schema.define(:version => 20130111131115) do
 
   create_table "categories", :force => true do |t|
     t.string   "name",       :null => false
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "category_pictures", :id => false, :force => true do |t|
+    t.integer "picture_id",  :null => false
+    t.integer "category_id", :null => false
+  end
+
+  create_table "pictures", :force => true do |t|
+    t.string   "name",        :null => false
+    t.text     "description"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
 
 end

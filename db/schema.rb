3f75700046c003ade5979b8290e3b115291bf29b
Tbo <thibault@official.fm>
2013-01-11 15:27:18 +0100

    Add Carrierwave for image processing

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-# -*- encoding : utf-8 -*-
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(:version => 20130111131115) do
   create_table "pictures", :force => true do |t|
     t.string   "name",        :null => false
     t.text     "description"
+    t.string   "file",        :null => false
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
   end

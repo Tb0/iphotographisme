64e1fb907e6d23c81ea66f7ac14e2da6dded7fc0
Tbo <thibault@official.fm>
2013-01-11 13:32:29 +0100

    First commit

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.9'
+gem 'jquery-rails', '2.1.4'
+gem 'pg', '0.14.1'
+
+group :assets do
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :test do
+  gem 'mocha',              '0.13.1', :require => false
+  gem 'factory_girl_rails', '4.1.0'
+  gem 'minitest',           '4.4.0'
+end
+
+group :development do
+  gem 'magic_encoding', '0.0.2'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IphotographismeV2::Application.config.secret_token = '3d49de0569c7d5b897d5afc2ca5bbda40168e370e263dd76adb70ae2cc43e69aca7bb0683e34a837fd2266229e8ff28d15bf66cccfa9fa49601214a81a68d1ea'

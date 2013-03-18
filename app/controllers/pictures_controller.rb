809bfac51da99b990007577af1fed10de9d5ec39
Tbo <thibault@official.fm>
2013-03-18 14:40:08 +0100

    Conflict between kaminari and will paginate per_page method

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -2,7 +2,7 @@
 class PicturesController < ApplicationController
 
 	def index
-		@pictures = Picture.order("created_at").page(params[:page]).per_page(8)
+		@pictures = Picture.order("created_at").page(params[:page]).per_page(20)
 		@categories = Category.all
 	end
 
